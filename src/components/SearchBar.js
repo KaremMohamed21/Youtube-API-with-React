7355421d08a64050a49c4e34e0bc3beb211fc79a
Karem Mohamed <karemkero21@gmail.com>
2020-05-15 15:58:37 +0200

    Make Search Component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,30 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = {
+    searchKey: "",
+  };
+
   render() {
-    return <div>SearchBar</div>;
+    return (
+      <div className="ui segment">
+        <form
+          className="ui form"
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
+          <div className="field">
+            <label>Video search</label>
+            <input
+              type="text"
+              value={this.state.searchKey}
+              onChange={(e) => this.setState({ searchKey: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
